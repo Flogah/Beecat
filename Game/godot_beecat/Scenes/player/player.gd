34b46d3d44da2d197bcb27589c8c018fe55b44c7
Mohito <89162350+Flogah@users.noreply.github.com>
2025-08-14 13:45:13 +0200

    added finer jump controls; added more cat animations

--- Game/godot_beecat/Scenes/player/player.gd
+++ Game/godot_beecat/Scenes/player/player.gd
@@ -2,39 +2,44 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+
+const GRAVITY_MULTIPLIER = 2.0
+
+@export var jump_height:float = 50
+
+var jump_velocity:float
+var jumps:int = 2
+var jumps_left:int = 1
 
 var jump_buffer_time: float = 0.1
 var jump_buffer_timer: float = 0.0
-
-var coyote_time:float = 0.5
+var coyote_time:float = 0.1
 var coyote_timer:float = 0.0
 
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var gravity :Vector2 = Vector2(0, ProjectSettings.get_setting("physics/2d/default_gravity"))
 
 func _ready() -> void:
 	set_collision_mask_value(3, true) #for one way platforms
-	jump_buffer_timer = 0
-	coyote_timer = 0
+	jump_velocity = -(sqrt(jump_height * 2.0 * gravity.y))
 
 func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("jump") and coyote_timer > 0:
-		jump_buffer_timer = jump_buffer_time
 		jump()
+		coyote_timer = -1
+
+	if Input.is_action_just_released("jump"):
+		velocity.y *= 0.5
 
 func _process(delta: float) -> void:
-	if jump_buffer_timer > 0: jump_buffer_timer -= delta
-	if coyote_timer > 0: coyote_timer -= delta
+	coyote_timer -= delta
 	
 	
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
 
-	if is_on_floor():
-		coyote_timer = coyote_time
+		
+		
 	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -57,9 +62,23 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_released("down"):
 		set_collision_mask_value(3, true)
 	
+		# Add the gravity.
+	if not is_on_floor():
+		if velocity.y > 0:
+			velocity += gravity * GRAVITY_MULTIPLIER * delta
+			sprite.play("fall")
+		else:
+			velocity += gravity * delta
+			sprite.play("jump")
+			
+	if is_on_floor():
+		coyote_timer = coyote_time
+		jumps_left = jumps
+	
 	move_and_slide()
 
 
 func jump() -> void:
-	if jump_buffer_timer > 0 or coyote_timer > 0:
-		velocity.y = JUMP_VELOCITY
+	if jumps_left > 0:
+		velocity.y = jump_velocity
+		if jumps_left > 0 : jumps_left   -= 1
