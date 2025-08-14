315461c7a460fa6f90be358009875614b3de4423
Mohito <89162350+Flogah@users.noreply.github.com>
2025-08-14 16:42:35 +0200

    added a looking ahead functionality to the camera

--- Game/godot_beecat/Scenes/camera_2d.gd
+++ Game/godot_beecat/Scenes/camera_2d.gd
@@ -1,10 +1,19 @@
 extends Camera2D
 
-@export var focus: Node
+@export var target: Node
 
-@export var move_speed:float = 0.1
+@export var move_speed:float = 0.05
 
+func _ready() -> void:
+	if target: position = target.position
 
 func _physics_process(delta: float) -> void:
-	if focus:
-		position = position.lerp(focus.position, move_speed)
+	if target:
+		if target.is_in_group("player"):
+			var target_velocity = target.velocity
+			var h_look_ahead = clampf(target_velocity.x, -100, 100)
+			var v_look_ahead = clampf(target_velocity.y, -50, 50)
+			var look_ahead_vector = Vector2(h_look_ahead, v_look_ahead)
+			position = position.lerp(target.position + look_ahead_vector, move_speed)
+		else: position = position.lerp(target.position, move_speed * 2)
+	
