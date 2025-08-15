14b9d388c088e9580463a55f0961732ed094c077
Mohito <89162350+Flogah@users.noreply.github.com>
2025-08-15 22:59:16 +0200

    finished level 1

--- Game/godot_beecat/Scenes/lo.gd
+++ Game/godot_beecat/Scenes/lo.gd
@@ -1,3 +1,22 @@
 extends StaticBody2D
 
+@export var destruction_time:float = 10.0
+
 @onready var goose_detector: Area2D = $GooseDetector
+@onready var destruction_timer: Timer = $DestructionTimer
+
+
+
+func _on_goose_detector_body_entered(body: Node2D) -> void:
+	if body.is_in_group("goose"):
+		destruction_timer.start()
+
+
+func _on_goose_detector_body_exited(body: Node2D) -> void:
+	if destruction_timer.time_left > 0:
+		destruction_timer.stop()
+		destruction_timer.wait_time = destruction_time
+
+
+func _on_destruction_timer_timeout() -> void:
+	queue_free()
